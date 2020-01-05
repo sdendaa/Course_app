6cecb40e16090f9c448d38506c331b34b82dc8b3
Shelema Bekele <sdendaa@gmail.com>
2020-01-04 21:55:48 -0500

    Initial commit

---
+++ src/test/java/com/dendaa/discovery/gateway/CourseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dendaa.discovery.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
